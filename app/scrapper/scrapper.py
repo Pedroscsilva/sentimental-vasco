a9e4a966707043fe91bdfbffac42759091039799
Pedroscsilva <pedroscsilva2001@gmail.com>
2024-06-28 12:16:54 -0300

    feat: making commentaries work

--- app/scrapper/scrapper.py
+++ app/scrapper/scrapper.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver import Chrome
+# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
 import time
 
 basic_url = "https://www.supervasco.com"
@@ -38,20 +42,23 @@ def get_specific_new(additional_href):
             print(html.text)
 
 def get_commentaries(additional_href):
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    
-    with Chrome(options=chrome_options) as browser:
-        browser.get(f"{basic_url}{additional_href}")
+    options = Options()
+    # options.add_argument('-headless')
+    firefox_profile = FirefoxProfile()
+    firefox_profile.set_preference('javascript.enabled', True)
+    options.profile = firefox_profile
 
-        # WebDriverWait(browser, 20).until(
-        #     EC.presence_of_element_located((By.CLASS_NAME, "pb-3"))
-        # )
+    with Firefox(options=options) as driver:
+        driver.get(f"{basic_url}{additional_href}")
+        
+        element = driver.find_element(By.CLASS_NAME, 'comment-tabs')
+        driver.execute_script("arguments[0].scrollIntoView();", element)
 
-        # Get the updated page source
-        html = browser.page_source
+        driver.implicitly_wait(2)
+        iframe = driver.find_element(By.ID, 'id_iframe_comment')
+        driver.switch_to.frame('id_iframe_comment')
+
+        html = driver.page_source
         soup = BeautifulSoup(html, "html.parser")
 
     with open('file.html', 'w') as file:

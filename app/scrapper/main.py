b19cda73596d01621d5284afd11bab7f6f2915d3
Pedroscsilva <pedroscsilva2001@gmail.com>
2024-07-13 17:52:27 +0200

    feat: transforming scrapper file into object

--- app/scrapper/main.py
+++ app/scrapper/main.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
 
-from scrapper import get_all_news, get_specific_new, get_commentaries
+from scrapper import VascoScrapper
 from simple_term_menu import TerminalMenu
 
 
 def main():
-    all_news = get_all_news()
+    vs = VascoScrapper()
+    all_news = vs.get_all_news()
     options = [d.get("title", None) for d in all_news]
     terminal_menu = TerminalMenu(options)
     menu_entry_index = terminal_menu.show()
-    get_specific_new(all_news[menu_entry_index]["href"])
-    get_commentaries('/noticias/feminino-registros-do-treino-desta-quinta-27-394936.html')
+    vs.get_news_data(all_news[menu_entry_index]["href"])
+    vs.get_commentaries('/noticias/feminino-registros-do-treino-desta-quinta-27-394936.html')
     input()
     main()
 
--- app/scrapper/scrapper.py
+++ app/scrapper/scrapper.py
@@ -10,79 +10,87 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 import time
+import pandas as pd
 
-basic_url = "https://www.supervasco.com"
+class VascoScrapper:
+    def __init__(self):
+        self.basic_url = "https://www.supervasco.com"
+        self.obtained_page = pd.DataFrame(columns=['index', 'datetime', 'title', 'link'])
+        self.commentaries = pd.DataFrame(columns=['index', 'commentary_source', 'commentary'])
+        
 
-
-def get_all_news():
-    page = requests.get(basic_url)
-    html_content = page.text
-
-    soup = BeautifulSoup(html_content, "html.parser")
-
-    page_titles = soup.select("h4 a")
-
-    page_attrs = map(lambda title: title.attrs, page_titles)
-
-    return list(page_attrs)
+# basic_url = "https://www.supervasco.com"
 
 
-def get_specific_new(additional_href):
-    page = requests.get(f"{basic_url}{additional_href}")
-    html_content = page.text
+    def get_all_news(self):
+        page = requests.get(self.basic_url)
+        html_content = page.text
 
-    soup = BeautifulSoup(html_content, "html.parser")
+        soup = BeautifulSoup(html_content, "html.parser")
 
-    page_title = soup.select_one("h1").text
-    page_text_html = soup.select("#lightgallery > p")
+        page_titles = soup.select("h4 a")
 
-    print(f"\n{page_title}\n")
-    for html in page_text_html:
-        if html.text != "":
-            print(html.text)
+        page_attrs = map(lambda title: title.attrs, page_titles)
+
+        return list(page_attrs)
 
 
-def get_extracted_page_data(driver):
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
+    def get_news_data(self, additional_href):
+        page = requests.get(f"{self.basic_url}{additional_href}")
+        html_content = page.text
 
-    print(html.page_title)
+        soup = BeautifulSoup(html_content, "html.parser")
+
+        page_title = soup.select_one("h1").text
+        page_text_html = soup.select("#lightgallery > p")
+
+        print(f"\n{page_title}\n")
+        for html in page_text_html:
+            if html.text != "":
+                print(html.text)
 
 
-def get_facebook_commentaries(driver):
-    iframe = driver.find_element(By.CSS_SELECTOR, 'iframe[data-testid="fb:comments Facebook Social Plugin"]')
-    driver.switch_to.frame(iframe)
-    WebDriverWait(driver, 6).until(
-        EC.presence_of_element_located((By.CLASS_NAME, '_5mdd'))
-    )
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    tags = soup.select('._5mdd span')
-    return [('facebook', tag.get_text(separator=' ')) for tag in tags]
+    def get_extracted_page_data(self, driver):
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
 
-def get_supervasco_commentaries(driver):
-    driver.switch_to.frame('id_iframe_comment')
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
+        print(html.page_title)
 
-    tags = soup.select('.pb-3 > p')
-    return [('supervasco', tag.text) for tag in tags]
 
-def get_commentaries(additional_href):
-    options = Options()
-    options.add_argument('-headless')
+    def get_facebook_commentaries(self, driver):
+        iframe = driver.find_element(By.CSS_SELECTOR, 'iframe[data-testid="fb:comments Facebook Social Plugin"]')
+        driver.switch_to.frame(iframe)
+        WebDriverWait(driver, 6).until(
+            EC.presence_of_element_located((By.CLASS_NAME, '_5mdd'))
+        )
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        tags = soup.select('._5mdd span')
+        return [('facebook', tag.get_text(separator=' ')) for tag in tags]
 
-    with Firefox(options=options) as driver:
-        driver.get(f"{basic_url}{additional_href}")
-        try:
-            fb = get_facebook_commentaries(driver)
-        except:
-            fb = []
-        driver.switch_to.parent_frame()
-        vs = get_supervasco_commentaries(driver)
-        print(fb)
-        print('\n')
-        print(vs)
+    def get_supervasco_commentaries(self, driver):
+        driver.switch_to.frame('id_iframe_comment')
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
 
-    
-    print('done')
+        tags = soup.select('.pb-3 > p')
+        return [('supervasco', tag.text) for tag in tags]
+
+    def get_commentaries(self, additional_href):
+        options = Options()
+        options.add_argument('-headless')
+
+        with Firefox(options=options) as driver:
+            driver.get(f"{self.basic_url}{additional_href}")
+            try:
+                fb = self.get_facebook_commentaries(driver)
+            except:
+                fb = []
+            driver.switch_to.parent_frame()
+            vs = self.get_supervasco_commentaries(driver)
+            print(fb)
+            print('\n')
+            print(vs)
+
+        
+        print('done')
